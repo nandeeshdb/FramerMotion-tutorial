b4606bb4aad67122220369f8a2cece1e8693d9d2
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-12-20 16:13:18 +0530

    filr stucture

---
+++ app/(root)/components/BasicMotion.jsx
@@ -0,0 +1,12 @@
+
+function BasicMotion() {
+  return (
+    <div className="flex w-full items-center justify-center h-screen gap-3">
+        <div className="w-36 h-36 bg-black">
+
+        </div>
+    </div>
+  )
+}
+
+export default BasicMotion
---
+++ app/(root)/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BasicMotion from './components/BasicMotion'
+
+function MainPage() {
+  return (
+    <div>
+      <BasicMotion />
+    </div>
+  )
+}
+
+export default MainPage
