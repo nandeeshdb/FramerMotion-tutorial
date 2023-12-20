e427225639f03d922a96458e374949f066132362
nandeeshdb <nandeeshbasavarajaiah@gmail.com>
2023-12-20 17:02:07 +0530

    AnimationPresence and exit props

--- app/(root)/components/BasicMotion.jsx
+++ app/(root)/components/BasicMotion.jsx
@@ -1,22 +1,42 @@
 "use client"
-import {motion} from 'framer-motion'
+import {motion,AnimatePresence} from 'framer-motion'
+import { useState } from 'react'
 function BasicMotion() {
+    const [isVisible,setIsVisible] = useState(false)
   return (
-    <div className="flex w-full items-center justify-center h-screen gap-3">
+    <div className="flex flex-col w-full items-center justify-center h-screen gap-3">
+        <button className='p-3 bg-blue-400 ' onClick={()=>setIsVisible(!isVisible)} layout>Show/Hide</button>
+        <AnimatePresence mode='popLayout'>
+        {isVisible && (
         <motion.div 
         initial={{
-            rotate:"0deg"
+            rotate:"0deg",
+            scale:0,
+            y:0
         }}
         animate={{
-            rotate:"180deg"
+            rotate:"180deg",
+            scale:1,
+            y:[0,150,-150,-150,150,0]
+        }}
+        exit={{
+            rotate:"0deg",
+            scale:0,
+            y:0
+            
         }}
 
         transition={{
             duration:1,
-            ease:'anticipate'
+            ease:'anticipate',
+            
         }}
+
+      
         className="w-36 h-36 bg-black">
         </motion.div>
+        )}
+        </AnimatePresence>
     </div>
   )
 }
